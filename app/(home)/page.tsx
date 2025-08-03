9c08019c71123ac562c987dc2d2b0bdb60561025
Piyush Kumar Singh <a661piyush@gmail.com>
2025-08-03 14:45:18 +0530

    update

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
       <footer className="w-full border-t border-gray-300 dark:border-white/20 mt-10 py-6 px-6 bg-gradient-to-r from-white to-gray-100 dark:from-neutral-900 dark:to-neutral-800">
   <div className="max-w-6xl mx-auto sm:ml-4 md:ml-12 flex flex-col sm:flex-row justify-between items-center text-sm text-gray-400 dark:text-neutral-300">
           <div className="mb-3 sm:mb-0 text-center sm:text-left">
-            © {new Date().getFullYear()} Sonu Rai — Full Stack Developer{" "}
+            © {new Date().getFullYear()} Piyush Kumar Singh — Full Stack Developer{" "}
             <br className="sm:hidden" />
             Blending functionality with beautiful UI to build seamless digital
             experiences.
